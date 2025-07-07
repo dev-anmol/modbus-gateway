014171ba01a1fe356a9a941e64e42a06fa128e40
Anmol Thakur <anmolll.thakurrr@gmail.com>
2025-07-07 18:25:22 +0530

    updated

--- src/components/add-device/add-device.ts
+++ src/components/add-device/add-device.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal, WritableSignal } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MessageService } from 'primeng/api';
 import { ToastModule } from 'primeng/toast';
@@ -13,45 +13,62 @@ import { ToastModule } from 'primeng/toast';
 export class AddDevice {
   private messageService = inject(MessageService);
 
+  private deviceName : WritableSignal<string> = signal('Device Name');
+  private deviceId : WritableSignal<string> = signal('Device Id');
+  private samplingInterval : WritableSignal<string> = signal('Sampling Interval');
+  private ipAddress : WritableSignal<string> = signal('Ip Address');
+  private mode : WritableSignal<string> = signal('Mode');
+
   deviceForm = new FormGroup({
-    id: new FormControl<number | null>(null),
+    id: new FormControl<string | null>(null),
     deviceName: new FormControl<string | null>(null),
     ipAddress: new FormControl<string | null>(null),
     mode: new FormControl<string | null>(null),
-    samplingInterval: new FormControl<number | null>(null),
-    timeout: new FormControl<number | null>(null),
+    samplingInterval: new FormControl<string | null>(null),
+    timeout: new FormControl<string | null>(null),
   });
 
   handleFormData() {
     console.log(this.deviceForm.value, 'and testing ng prime');
-    if (this.deviceForm.value.deviceName === null) {
-      this.generateToast('Please Enter the Device Name');
+    if (
+      this.deviceForm.value.deviceName === null ||
+      this.deviceForm.value.deviceName === ''
+    ) {
+      this.generateToast('Please Enter the Device Name', this.deviceName);
     }
 
-    if (this.deviceForm.value.id === null) {
-      this.generateToast('Please Enter the Device Id');
+    if (this.deviceForm.value.id === null || this.deviceForm.value.id === '') {
+      this.generateToast('Please Enter the Device Id', this.deviceId);
     }
 
-    if (this.deviceForm.value.ipAddress === null) {
-      this.generateToast('Please Enter the IP Address');
+    if (
+      this.deviceForm.value.ipAddress === null ||
+      this.deviceForm.value.ipAddress === ''
+    ) {
+      this.generateToast('Please Enter the IP Address', this.ipAddress);
     }
 
-    if (this.deviceForm.value.mode === null) {
-      this.generateToast('Please Enter the Transmission Mode');
+    if (
+      this.deviceForm.value.mode === null ||
+      this.deviceForm.value.mode === ''
+    ) {
+      this.generateToast('Please Enter the Transmission Mode', this.mode);
     }
 
-    if (this.deviceForm.value.samplingInterval === null) {
-      this.generateToast('Please Enter the Sampling Interval');
+    if (
+      this.deviceForm.value.samplingInterval === null ||
+      this.deviceForm.value.samplingInterval === ''
+    ) {
+      this.generateToast('Please Enter the Sampling Interval', this.samplingInterval);
     }
   }
 
-  generateToast(msg: string) {
+  generateToast(msg: string, key: WritableSignal<string>) {
     this.messageService.add({
       severity: 'warn',
       summary: msg,
       detail: 'Invalid Fields',
       life: 3000,
-      sticky: true,
       closable: true,
     });
   }
--- src/components/modbus-server/modbus-server.ts
+++ src/components/modbus-server/modbus-server.ts
@@ -1,27 +1,82 @@
-import { Component } from '@angular/core';
-import {
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule
-} from '@angular/forms';
+import { Component, inject, signal, WritableSignal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MessageService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-modbus-server',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, ToastModule],
   templateUrl: './modbus-server.html',
   styleUrl: './modbus-server.css',
+  providers: [MessageService],
 })
 export class ModbusServer {
   serverForm = new FormGroup({
     serverIpAddress: new FormControl<string | null>(null),
-    serverPort: new FormControl<number | null>(null),
-    poolSize: new FormControl<number | null>(null),
-    unitId: new FormControl<number | null>(null),
-    interval: new FormControl<number | null>(null),
+    serverPort: new FormControl<string | null>(null),
+    poolSize: new FormControl<string | null>(null),
+    unitId: new FormControl<string | null>(null),
+    interval: new FormControl<string | null>(null),
   });
 
-  handleFormData() {
-    console.log(this.serverForm.value)
+  private messageService = inject(MessageService);
+  private serverIpAddress: WritableSignal<string> = signal('Server Ip Address');
+  private serverPort: WritableSignal<string> = signal('Server Port');
+  private poolSize: WritableSignal<string> = signal('Pool Size');
+  private unitId: WritableSignal<string> = signal('Unit Id');
+  private interval: WritableSignal<string> = signal('Interval');
 
+  handleFormData() {
+    console.log(this.serverForm.value, 'and testing ng prime');
+    if (
+      this.serverForm.value.serverIpAddress === null ||
+      this.serverForm.value.serverIpAddress === ''
+    ) {
+      this.generateToast(
+        'Please Enter Server Ip Address',
+        this.serverIpAddress
+      );
+    }
+
+    if (
+      this.serverForm.value.unitId === null ||
+      this.serverForm.value.unitId === ''
+    ) {
+      this.generateToast('Please Enter the Unit Id', this.unitId);
+    }
+
+    if (
+      this.serverForm.value.serverPort === null ||
+      this.serverForm.value.serverPort === ''
+    ) {
+      this.generateToast('Please Enter the Server Port', this.serverPort);
+    }
+
+    if (
+      this.serverForm.value.poolSize === null ||
+      this.serverForm.value.poolSize === ''
+    ) {
+      this.generateToast(
+        'Please Enter the Transmission poolSize',
+        this.poolSize
+      );
+    }
+
+    if (
+      this.serverForm.value.interval === null ||
+      this.serverForm.value.interval === ''
+    ) {
+      this.generateToast('Please Enter the Sampling Interval', this.interval);
+    }
+  }
+
+  generateToast(msg: string, key: WritableSignal<string>) {
+    this.messageService.add({
+      severity: 'warn',
+      summary: msg,
+      detail: 'Invalid Fields',
+      life: 3000,
+      closable: true,
+    });
   }
 }
