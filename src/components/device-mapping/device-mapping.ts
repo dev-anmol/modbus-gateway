4eeb4da100076862b5a926ac8fe5d602d9947b0e
Anmol Thakur <anmolll.thakurrr@gmail.com>
2025-07-08 16:03:30 +0530

    updated

--- src/components/device-mapping/device-mapping.ts
+++ src/components/device-mapping/device-mapping.ts
@@ -7,19 +7,30 @@ import { registers } from '../../models/register.type';
   styleUrl: './device-mapping.css',
 })
 export class DeviceMapping {
-  public register: WritableSignal<registers> = signal([
+  public registers: WritableSignal<registers> = signal([
     'HOLDING_REGISTERS',
     'INPUT_REGISTERS',
     'DISCRETE_INPUTS',
     'COILS',
   ]);
 
-  public header : WritableSignal<string[]> = signal([
-    "id", 
-    "Parameter",
-    "Register Address",
-    "Register Type",
-    "Data Type",
-    "Interval"
-  ])
+  public header: WritableSignal<string[]> = signal([
+    'Id',
+    'Parameter',
+    'Register Address',
+    'Register Type',
+    'Data Type',
+    'Interval',
+  ]);
+
+  public dataTypes: WritableSignal<string[]> = signal([
+    'BOOLEAN',
+    'INT16',
+    'UINT16',
+    'INT32',
+    'UINT32',
+    'LONG',
+    'FLOAT',
+    'DOUBLE',
+  ]);
 }
