9c1a1fe7f471f76abce05b16d169ea09a1be038d
Anmol Thakur <anmolll.thakurrr@gmail.com>
2025-07-10 15:23:28 +0530

    updated

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 // require('./db/config');
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 3000;
 
-
+app.use(cors());
 app.use(express.json());
 
 
@@ -14,7 +14,14 @@ app.get('/', (req, res) => {
     })
 })
 
+app.use('/api/devices', require('./routes/deviceRoutes'));
+app.use('/api/device-profile', require('./routes/profileRoutes'));
+app.use('/api/address-maps', require('./routes/addressMapRoutes'));
 
-app.listen(port, () => {
+
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
     console.log(`Server running on port ${port}`)
 })
