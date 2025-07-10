361ee0ec4427996d008ef2d326a2d7289ba9ec5d
Anmol Thakur <anmolll.thakurrr@gmail.com>
2025-07-10 15:28:25 +0530

    updated

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,17 +8,9 @@ app.use(cors());
 app.use(express.json());
 
 
-app.get('/', (req, res) => {
-    res.status(200).json({
-        msg: "hi"
-    })
-})
-
-app.use('/api/devices', require('./routes/deviceRoutes'));
-app.use('/api/device-profile', require('./routes/profileRoutes'));
-app.use('/api/address-maps', require('./routes/addressMapRoutes'));
-
-
+app.use('/api/devices', require('./routes/device.routes'));
+app.use('/api/device-profile', require('./routes/profile.routes'));
+app.use('/api/address-maps', require('./routes/addressmap.routes'));
 
 
 const PORT = process.env.PORT || 3000;
