72d1966dfe6f9c1fa2fa5dcb5efb228b46540e0e
Anmol Thakur <anmolll.thakurrr@gmail.com>
2025-07-21 18:14:28 +0530

    updated

--- openmuc/projects/app/modbusgateway/src/main/java/org/openmuc/framework/app/modbusgateway/dataholder/ModbusDataHolder.java
+++ openmuc/projects/app/modbusgateway/src/main/java/org/openmuc/framework/app/modbusgateway/dataholder/ModbusDataHolder.java
@@ -1,4 +1,88 @@
 package org.openmuc.framework.app.modbusgateway.dataholder;
 
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.Map;
+
+/**
+ * Singleton class for holding Modbus data in memory.
+ * Stores Holding Registers, Input Registers, Coils, and Discrete Inputs.
+ */
 public class ModbusDataHolder {
+    private static final ModbusDataHolder INSTANCE = new ModbusDataHolder();
+
+    private final Map<String, Object> channelData = new ConcurrentHashMap<>();
+    private final Map<Integer, Integer> holdingRegisters = new ConcurrentHashMap<>();
+    private final Map<Integer, Integer> inputRegisters = new ConcurrentHashMap<>();
+    private final Map<Integer, Boolean> coils = new ConcurrentHashMap<>();
+    private final Map<Integer, Boolean> discreteInputs = new ConcurrentHashMap<>();
+
+    private ModbusDataHolder() {}
+
+    /**
+     * Get the singleton instance of ModbusDataHolder.
+     */
+    public static ModbusDataHolder getInstance() {
+        return INSTANCE;
+    }
+
+    // Channel data methods
+    public void setChannelData(String channelId, Object value) {
+        channelData.put(channelId, value);
+    }
+
+    public Object getChannelData(String channelId) {
+        return channelData.get(channelId);
+    }
+
+    // Holding Registers
+    public void setHoldingRegister(int address, int value) {
+        holdingRegisters.put(address, value);
+    }
+
+    public Integer getHoldingRegister(int address) {
+        return holdingRegisters.get(address);
+    }
+
+    public Map<Integer, Integer> getAllHoldingRegisters() {
+        return new ConcurrentHashMap<>(holdingRegisters);
+    }
+
+    // Input Registers
+    public void setInputRegister(int address, int value) {
+        inputRegisters.put(address, value);
+    }
+
+    public Integer getInputRegister(int address) {
+        return inputRegisters.get(address);
+    }
+
+    public Map<Integer, Integer> getAllInputRegisters() {
+        return new ConcurrentHashMap<>(inputRegisters);
+    }
+
+    // Coils
+    public void setCoil(int address, boolean value) {
+        coils.put(address, value);
+    }
+
+    public Boolean getCoil(int address) {
+        return coils.get(address);
+    }
+
+    public Map<Integer, Boolean> getAllCoils() {
+        return new ConcurrentHashMap<>(coils);
+    }
+
+    // Discrete Inputs
+    public void setDiscreteInput(int address, boolean value) {
+        discreteInputs.put(address, value);
+    }
+
+    public Boolean getDiscreteInput(int address) {
+        return discreteInputs.get(address);
+    }
+
+    public Map<Integer, Boolean> getAllDiscreteInputs() {
+        return new ConcurrentHashMap<>(discreteInputs);
+    }
 }
--- openmuc/projects/app/modbusgateway/src/main/java/org/openmuc/framework/app/modbusgateway/server/ModbusServer.java
+++ openmuc/projects/app/modbusgateway/src/main/java/org/openmuc/framework/app/modbusgateway/server/ModbusServer.java
@@ -1,5 +1,26 @@
 package org.openmuc.framework.app.modbusgateway.server;
+import org.openmuc.framework.data.Record;
+import org.openmuc.framework.data.Value;
+import org.openmuc.framework.app.modbusgateway.pojo.Device;
+import org.openmuc.framework.app.modbusgateway.pojo.Mapping;
+import org.openmuc.framework.app.modbusgateway.pojo.Profile;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import com.ghgande.j2mod.modbus.ModbusException;
+import com.ghgande.j2mod.modbus.procimg.DefaultProcessImageFactory;
+import com.ghgande.j2mod.modbus.procimg.IllegalAddressException;
+import com.ghgande.j2mod.modbus.procimg.ProcessImage;
+import com.ghgande.j2mod.modbus.procimg.Register;
+import com.ghgande.j2mod.modbus.procimg.SimpleInputRegister;
+import com.ghgande.j2mod.modbus.procimg.SimpleRegister;
+import com.ghgande.j2mod.modbus.slave.ModbusSlave;
+import com.ghgande.j2mod.modbus.slave.ModbusSlaveFactory;
+import com.ghgande.j2mod.modbus.util.SerialParameters;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ModbusServer {
 
